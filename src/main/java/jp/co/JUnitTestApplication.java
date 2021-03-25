fe6e9d740e519c27ae1cff776f932f7d665295e2
OH JIWON <ojw970725@gmail.com>
2021-03-25 16:09:36 +0900

    최초 커밋

---
+++ src/main/java/jp/co/JUnitTestApplication.java
@@ -0,0 +1,13 @@
+package jp.co;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JUnitTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JUnitTestApplication.class, args);
+	}
+
+}
